0e6e2ebe54a19bd80fee879d0ffe44bd49b3e46d
deepak <dbhatt@kabloomcorp.com>
2019-05-09 11:46:56 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component{
+   
+    state = {lat: null,long: null,errMsg: '', timeStamp: ''};
+    componentDidMount() {
+    this.getLocation();
+    }
+    getLocation () {
+        window.navigator.geolocation.getCurrentPosition(
+                    ( position ) => {
+                        this.setState({lat:position.coords.latitude,long:position.coords.longitude});
+
+                        setInterval(()=>{
+                            this.setState({ timeStamp: new Date().toLocaleTimeString() });
+                        },1000);
+                                                          
+                        
+                    }
+                    ,( err ) => this.setState({errMsg: err.message})
+                                );
+    
+    }
+    render () {
+        if( !this.state.errMsg && this.state.lat && this.state.long ) {
+            return (                     
+            <div>
+                <SeasonDisplay lat={this.state.lat}  long={this.state.long} error={this.state.errMsg} time={this.state.timeStamp}/>
+               
+            </div>
+            );
+        }
+        if( !this.state.errMsg && !this.state.lat && !this.state.long  ) {
+        return (   
+            <Spinner message="please accept location request" /> 
+             );
+        }
+        return (
+            <div>
+                {!this.state.errMsg ? '' :  <span>{this.state.errMsg}</span>}
+            </div>
+        );
+                
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
